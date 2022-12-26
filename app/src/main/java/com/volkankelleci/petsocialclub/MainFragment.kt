0bbfa8192202359fdf95eee55a7bf2be2211c55f
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2022-12-26 22:23:59 +0300

    Codes transported to other Fragment

--- app/src/main/java/com/volkankelleci/petsocialclub/MainFragment.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/MainFragment.kt
@@ -1,11 +1,11 @@
 package com.volkankelleci.petsocialclub
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_main.*
@@ -14,7 +14,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
-import kotlin.math.sign
 
 
 class MainFragment : Fragment() {
@@ -32,36 +31,19 @@ class MainFragment : Fragment() {
         // Inflate the layout for this fragment
 
         auth=FirebaseAuth.getInstance()
-
+        
         return inflater.inflate(R.layout.fragment_main, container, false)
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        loginButton.setOnClickListener {
-            signUser()
-
+        signUpText.setOnClickListener {
+            val action=MainFragmentDirections.actionMainFragmentToSignUpFragment()
+            findNavController().navigate(action)
         }
     }
 
-    fun signUser(){
-        val email=etUserName.text.toString()
-        val password=etPassword.text.toString()
 
-        if (email.isNotEmpty() && password.isNotEmpty()){
-            CoroutineScope(Dispatchers.IO).launch {
-                try {
-                    auth.signInWithEmailAndPassword(email, password).await()
-                    withContext(Dispatchers.Main){
-                        Toast.makeText(context,"SIGN IS DONE", Toast.LENGTH_LONG).show()
-                    }
-
-                }catch (e:Exception){
-                    e.printStackTrace()
-                }
-            }
-        }
-    }
 
 }
--- app/src/main/java/com/volkankelleci/petsocialclub/SignUpFragment.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/SignUpFragment.kt
@@ -5,28 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_main.*
+import kotlinx.android.synthetic.main.fragment_main.etPassword
+import kotlinx.android.synthetic.main.fragment_main.etUserName
+import kotlinx.android.synthetic.main.fragment_sign_up.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import kotlin.math.sign
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SignUpFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignUpFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -34,26 +30,47 @@ class SignUpFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
+
+        auth=FirebaseAuth.getInstance()
+
+
+
         return inflater.inflate(R.layout.fragment_sign_up, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignUpFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignUpFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        signUpButton.setOnClickListener {
+            signUser()
+        }
+    }
+
+    fun signUser(){
+        val email=etUserName.text.toString()
+        val password=etPassword.text.toString()
+
+        if (email.isNotEmpty() && password.isNotEmpty()){
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    auth.createUserWithEmailAndPassword(email, password).await()
+                    withContext(Dispatchers.Main){
+                        checkLoggedInState()
+                    }
+
+                }catch (e:Exception){
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(context,e.message, Toast.LENGTH_LONG).show()
+                    }
                 }
             }
+        }
+    }
+    private fun checkLoggedInState(){
+        if(auth.currentUser==null){
+            Toast.makeText(context,"SIGN IS UNSUCCESS", Toast.LENGTH_LONG).show()
+        }else{
+            Toast.makeText(context,"SIGN DONE", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
