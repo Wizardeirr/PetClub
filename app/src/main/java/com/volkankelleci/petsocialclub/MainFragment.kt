611e470caa3167552d809c0929a55a5bb602404d
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2022-12-27 11:58:59 +0300

    Google depend added gradle

--- app/src/main/java/com/volkankelleci/petsocialclub/MainFragment.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
@@ -50,6 +51,8 @@ class MainFragment : Fragment() {
                 .requestIdToken(getString(R.string.webclient_id))
                 .requestEmail()
                 .build()
+            val signer=GoogleSignIn.getClient(requireActivity(),options)
+
 
         }
 
