3b0cd34d613994255df82f5d8124f8ddd35b6d31
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2022-12-29 18:08:48 +0300

    Data added for FB

---
+++ app/src/main/java/com/volkankelleci/petsocialclub/data/UsersData.kt
@@ -0,0 +1,12 @@
+package com.volkankelleci.petsocialclub.data
+
+data class UsersData(
+
+    var petName:String,
+    var petAge: String,
+    var petSpecies:String,
+    var petKg:String,
+    var petGender:String,
+    var vaccineInfo:String,
+    var ownerName:String
+)
