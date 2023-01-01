0b340b0c80059fa7321dbe18f0bffedc5cea5171
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2023-01-01 15:08:55 +0300

    RecyclerAdapter created

---
+++ app/src/main/java/com/volkankelleci/petsocialclub/adapter/UserRecyclerAdapter.kt
@@ -0,0 +1,24 @@
+package com.volkankelleci.petsocialclub.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class UserRecyclerAdapter(): RecyclerView.Adapter<UserRecyclerAdapter.UserViewHolder>() {
+    class UserViewHolder(itemView:View):RecyclerView.ViewHolder(itemView) {
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
