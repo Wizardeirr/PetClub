3a8020e5df4c6dcca664b54385bb4272da50c8cb
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2023-01-06 13:26:25 +0300

    Glide done

--- app/src/main/java/com/volkankelleci/petsocialclub/util/Util.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/util/Util.kt
@@ -1,5 +1,11 @@
 package com.volkankelleci.petsocialclub.util
 
+import android.content.Context
+import android.media.Image
+import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
@@ -9,4 +15,16 @@ object Util {
      var storage:FirebaseStorage=FirebaseStorage.getInstance()
      var database:FirebaseFirestore=FirebaseFirestore.getInstance()
 
+     fun ImageView.downloadImageToRecycler(url:String,placeholder:CircularProgressDrawable){
+          val options=RequestOptions().placeholder(placeholder)
+          Glide.with(context).setDefaultRequestOptions(options).load(url).into(this)
+     }
+     fun createPlaceHolder(context: Context):CircularProgressDrawable{
+          return CircularProgressDrawable(context).apply {
+               strokeWidth=8f
+               centerRadius=40f
+               start()
+          }
+
+     }
 }
