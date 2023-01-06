7bd01e87ba3ed3ba54c0bb2a14f9d2d10e38984d
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2023-01-06 15:41:11 +0300

    User Profile view creating

--- app/src/main/java/com/volkankelleci/petsocialclub/ProfileFragment.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/ProfileFragment.kt
@@ -7,11 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.auth.User
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import com.volkankelleci.petsocialclub.data.UsersData
+import com.volkankelleci.petsocialclub.util.Post
+import com.volkankelleci.petsocialclub.util.UserProfileInput
 import com.volkankelleci.petsocialclub.util.Util.auth
 import kotlinx.android.synthetic.main.fragment_message.*
 import kotlinx.android.synthetic.main.fragment_profile.*
@@ -20,7 +24,8 @@ import kotlinx.coroutines.tasks.await
 
 class ProfileFragment : Fragment() {
     private val userProfile= Firebase.firestore.collection("UserProfileInfos")
-    lateinit var data:UsersData
+
+    var UserProfileInfos=ArrayList<UserProfileInput>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -38,6 +43,38 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        takesData()
+    }
+    fun takesData(){
+        var database: FirebaseFirestore = FirebaseFirestore.getInstance()
+        database.collection("UserProfileInfos").orderBy("date", Query.Direction.DESCENDING)
+            .addSnapshotListener { value, error ->
+                if(error!=null){
+                }else
+                    if (value!=null){
+                        if (value.isEmpty==false){
+                            val documents=value.documents
+                            UserProfileInfos.clear()
+                            for (document in documents){
+                                document.get("Post")
+                                val ownerName=document.get("ownerName").toString()
+                                val petAge=document.get("petAge").toString()
+                                val petGender=document.get("petGender").toString()
+                                val petKg=document.get("petKg").toString()
+                                val petName=document.get("petName").toString()
+                                val petSpecies=document.get("petSpecies").toString()
+                                val vaccineInfo=document.get("vaccineInfo").toString()
+
+                                val downloadInfos= UserProfileInput(ownerName,petAge,petGender,petKg,petName,petSpecies,vaccineInfo)
+                                UserProfileInfos.add(downloadInfos)
+
+
+
+                            }
+
+                        }
+                    }
+            }
     }
 
 }
---
+++ app/src/main/java/com/volkankelleci/petsocialclub/util/UserProfileInput.kt
@@ -0,0 +1,14 @@
+package com.volkankelleci.petsocialclub.util
+
+class UserProfileInput(
+    var userOwnerName:String,
+    var userPetAge:String,
+    var userPetGender:String,
+    var userPetKg:String,
+    var userPetName:String,
+    var userPetSpecies:String,
+    var userVaccineInfo:String,
+
+
+    ) {
+}
