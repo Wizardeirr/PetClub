79597d49f38610a63d3651de98a2ea141c090dbc
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2023-01-06 16:08:28 +0300

    Recycler Raw binded

--- app/src/main/java/com/volkankelleci/petsocialclub/ProfileFragment.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/ProfileFragment.kt
@@ -7,24 +7,32 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.auth.User
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
+import com.volkankelleci.petsocialclub.adapter.ProfileAdapter
+import com.volkankelleci.petsocialclub.adapter.UserRecyclerAdapter
 import com.volkankelleci.petsocialclub.data.UsersData
+import com.volkankelleci.petsocialclub.databinding.FragmentProfileBinding
+import com.volkankelleci.petsocialclub.databinding.FragmentUsersHomeBinding
 import com.volkankelleci.petsocialclub.util.Post
 import com.volkankelleci.petsocialclub.util.UserProfileInput
 import com.volkankelleci.petsocialclub.util.Util.auth
 import kotlinx.android.synthetic.main.fragment_message.*
 import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.fragment_users_home.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.tasks.await
 
 class ProfileFragment : Fragment() {
     private val userProfile= Firebase.firestore.collection("UserProfileInfos")
-
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var recyclerViewAdapter:ProfileAdapter
     var UserProfileInfos=ArrayList<UserProfileInput>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,15 +43,20 @@ class ProfileFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         takesData()
+        var layoutManager= LinearLayoutManager(activity)
+        profileRecyclerView.layoutManager=layoutManager
+        recyclerViewAdapter= ProfileAdapter(UserProfileInfos)
+        profileRecyclerView.adapter=recyclerViewAdapter
     }
     fun takesData(){
         var database: FirebaseFirestore = FirebaseFirestore.getInstance()
---
+++ app/src/main/java/com/volkankelleci/petsocialclub/adapter/ProfileAdapter.kt
@@ -0,0 +1,37 @@
+package com.volkankelleci.petsocialclub.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.volkankelleci.petsocialclub.R
+import com.volkankelleci.petsocialclub.util.UserProfileInput
+import kotlinx.android.synthetic.main.profile_raw.view.*
+
+class ProfileAdapter(var userList:ArrayList<UserProfileInput>):RecyclerView.Adapter<ProfileAdapter.ProfileViewHolder>() {
+    class ProfileViewHolder(itemView:View):RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileViewHolder {
+        val inflater= LayoutInflater.from(parent.context)
+        val viewHolder=inflater.inflate(R.layout.profile_raw,parent,false)
+        return ProfileViewHolder(viewHolder)
+    }
+
+    override fun onBindViewHolder(holder: ProfileViewHolder, position: Int) {
+
+        holder.itemView.getOwnerName.text=userList[position].userOwnerName
+        holder.itemView.petAge.text=userList[position].userPetAge
+        holder.itemView.petGender.text=userList[position].userPetGender
+        holder.itemView.petKg.text=userList[position].userPetKg
+        holder.itemView.petSpecies.text=userList[position].userPetSpecies
+        holder.itemView.petVaccine.text=userList[position].userVaccineInfo
+        //image will add
+
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+}
