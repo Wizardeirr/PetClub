167e1b8cc99c4f1f1bfc9c578e379f2b457d7836
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2023-01-09 15:59:54 +0300

    chat recycler adapter almost done

--- app/src/main/java/com/volkankelleci/petsocialclub/UserChatFragment.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/UserChatFragment.kt
@@ -5,13 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.google.firebase.database.ktx.database
 import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.Query
 import com.google.firebase.ktx.Firebase
 import com.volkankelleci.petsocialclub.databinding.FragmentUserChatBinding
 import com.volkankelleci.petsocialclub.util.ChatData
 import com.volkankelleci.petsocialclub.util.Post
 import com.volkankelleci.petsocialclub.util.Util.auth
+import com.volkankelleci.petsocialclub.util.Util.database
 import java.util.UUID
 
 class UserChatFragment : Fragment() {
@@ -49,17 +52,40 @@ class UserChatFragment : Fragment() {
             chatDataMap.put("chatGUser",gUser)
             chatDataMap.put("chatGDate",gdate)
 
-            val allChatInfo=ChatData(gUserChatText,gUser,gdate)
-            chatList.add(allChatInfo)
-            val chatReference=db.getReference("UserChatCol")
-            chatReference.setValue(gUserChatText)
+            database.collection("Chats").add(chatDataMap).addOnSuccessListener {
+
+                binding.userChatText.setText("")
+            }.addOnFailureListener {
+                Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+            }
+
+            database.collection("Chats").orderBy("chatGDate",Query.Direction.ASCENDING).addSnapshotListener { value, error ->
+                if (error!=null){
+                    Toast.makeText(requireContext(), error.message, Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    if (value!=null){
+                        if (value.isEmpty){
+                            Toast.makeText(requireContext(), "Please Try Again", Toast.LENGTH_SHORT).show()
+
+                        }else{
+                            val documents=value.documents
+                            for (document in documents){
+                                val text=document.get("chatGText").toString()
+                                val user=document.get("chatGUser").toString()
+                                println(text)
+                            }
+                        }
+                    }
 
 
-
-
+                }
+            }
     }
 
+
 }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/volkankelleci/petsocialclub/adapter/ChatRecyclerAdapter.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/adapter/ChatRecyclerAdapter.kt
@@ -1,16 +1,39 @@
 package com.volkankelleci.petsocialclub.adapter
 
+import android.text.Layout
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.AsyncListUtil
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.volkankelleci.petsocialclub.R
+import com.volkankelleci.petsocialclub.util.ChatData
 
 class ChatRecyclerAdapter : RecyclerView.Adapter<ChatRecyclerAdapter.ChatRecyclerViewHolder>() {
     class ChatRecyclerViewHolder(itemView:View):RecyclerView.ViewHolder(itemView) {
 
     }
+    private val diffutil=object: DiffUtil.ItemCallback<ChatData>() {
+        override fun areItemsTheSame(oldItem: ChatData, newItem: ChatData): Boolean {
+            return oldItem==newItem
+        }
+
+        override fun areContentsTheSame(oldItem: ChatData, newItem: ChatData): Boolean {
+            return oldItem == newItem
+        }
+    }
+    private val recyclerListDiffUtil=AsyncListDiffer(this,diffutil)
+
+    var chats:List<ChatData>
+    get() =recyclerListDiffUtil.currentList
+        set(value)=recyclerListDiffUtil.submitList(value)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatRecyclerViewHolder {
-        TODO("Not yet implemented")
+
+        val view=LayoutInflater.from(parent.context).inflate(R.layout.)
+
     }
 
     override fun onBindViewHolder(holder: ChatRecyclerViewHolder, position: Int) {
@@ -18,6 +41,6 @@ class ChatRecyclerAdapter : RecyclerView.Adapter<ChatRecyclerAdapter.ChatRecycle
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return chats.size
     }
 }
