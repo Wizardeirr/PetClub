652a0c7abe3d40a30995cbd362127c982acfd610
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2023-01-12 15:27:37 +0300

    Adapter binded with Private_chat_raw

---
+++ app/src/main/java/com/volkankelleci/petsocialclub/adapter/PrivateChatAdapter.kt
@@ -0,0 +1,30 @@
+package com.volkankelleci.petsocialclub.adapter
+
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.volkankelleci.petsocialclub.R
+
+class PrivateChatAdapter: RecyclerView.Adapter<PrivateChatAdapter.PrivateChatAdapterViewHolder>() {
+    class PrivateChatAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): PrivateChatAdapterViewHolder {
+        val itemView=LayoutInflater.from(parent.context).inflate(R.layout.private_chat_raw,parent,false)
+        return PrivateChatAdapterViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: PrivateChatAdapterViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
