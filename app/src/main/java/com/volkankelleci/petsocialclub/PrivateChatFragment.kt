e4c78d2e5697ec758e6105cb67c9e96cbd090d85
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2023-01-12 15:30:18 +0300

    Private chat fragment VB done

--- app/src/main/java/com/volkankelleci/petsocialclub/PrivateChatFragment.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/PrivateChatFragment.kt
@@ -5,16 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.volkankelleci.petsocialclub.databinding.FragmentMessageBinding
+import com.volkankelleci.petsocialclub.databinding.FragmentUserChatBinding
 
 class PrivateChatFragment : Fragment() {
+
+    private  var _binding:FragmentUserChatBinding?=null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_private_chat, container, false)
+    ): View {
+        _binding = FragmentUserChatBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 }
