23bee498827b56909a5809d5c01876e8dee4aa3d
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2023-01-14 14:34:52 +0300

    USER PICTURE SAVED

--- app/src/main/java/com/volkankelleci/petsocialclub/doneviews/SignUpFragment.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/doneviews/SignUpFragment.kt
@@ -1,11 +1,22 @@
 package com.volkankelleci.petsocialclub.doneviews
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
@@ -13,16 +24,19 @@ import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.volkankelleci.petsocialclub.R
 import com.volkankelleci.petsocialclub.databinding.FragmentSignUpBinding
 import com.volkankelleci.petsocialclub.util.Util
 import com.volkankelleci.petsocialclub.util.Util.database
+import kotlinx.android.synthetic.main.fragment_message.*
 
 import kotlinx.android.synthetic.main.fragment_sign_up.*
 import kotlinx.android.synthetic.main.fragment_user_chat.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.selects.select
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 import java.util.*
@@ -34,6 +48,8 @@ class SignUpFragment : Fragment() {
     private lateinit var firestore: FirebaseFirestore
     private var _binding: FragmentSignUpBinding? = null
     private val binding get() = _binding!!
+    var selectedImage: Uri? = null
+    var selectedImageURI: Bitmap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         firestore = Firebase.firestore
@@ -56,6 +72,9 @@ class SignUpFragment : Fragment() {
             val action = SignUpFragmentDirections.actionSignUpFragmentToMainFragment()
             findNavController().navigate(action)
 
+        }
+        signImageView.setOnClickListener {
+            selectImage()
         }
     }
     fun signUser() {
@@ -86,19 +105,77 @@ class SignUpFragment : Fragment() {
         }
     }
     private fun userInfoTake() {
-        val userUUID = Util.auth.currentUser!!.uid
-        val userEmail = Util.auth.currentUser!!.email.toString()
-        val userName = binding.userNameET.text.toString()
-        val petName = binding.petName.text.toString()
+        val uuid = UUID.randomUUID()
+        val selectableImage = "${uuid}.jpg"
+        val storageRef = Util.storage.reference
+        val imageRef=storageRef.child("userProfileImages").child(selectableImage)
+        if (selectedImage != null) {
+            imageRef.putFile(selectedImage!!).addOnSuccessListener {taskSnapshot->
+                Toast.makeText(activity, "SUCCESS", Toast.LENGTH_SHORT).show()
+                val loadedImageReference = FirebaseStorage.getInstance()
+                    .reference.child("userProfileImages").child(selectableImage)
+                loadedImageReference.downloadUrl.addOnSuccessListener{uri->
+                    val userUUID = Util.auth.currentUser!!.uid
+                    val userEmail = Util.auth.currentUser!!.email.toString()
+                    val userName = binding.userNameET.text.toString()
+                    val petName = binding.petName.text.toString()
+                    val downloadImage = uri.toString()
 
-        val userInfoMap = HashMap<String, Any>()
+                    val userInfoMap = HashMap<String, Any>()
+                    userInfoMap.put("userImage",downloadImage)
+                    userInfoMap.put("userUUID", userUUID)
+                    userInfoMap.put("userEmail", userEmail)
+                    userInfoMap.put("userName", userName)
+                    userInfoMap.put("petName", petName)
+                    database.collection("userProfileInfo").add(userInfoMap).addOnSuccessListener {
+                        Toast.makeText(requireContext(), "DONE", Toast.LENGTH_SHORT).show()
+                    }
+                        .addOnFailureListener {
+                    }
 
-        userInfoMap.put("userUUID", userUUID)
-        userInfoMap.put("userEmail", userEmail)
-        userInfoMap.put("userName", userName)
-        userInfoMap.put("petName", petName)
-        database.collection("userProfileInfo").add(userInfoMap).addOnSuccessListener {
-        }.addOnFailureListener {
+                }
+
+    }
         }
+    }
+    private fun selectImage() {
+        if (ContextCompat.checkSelfPermission(requireContext(),
+                android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            ActivityCompat.requestPermissions(requireActivity(),
+                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                1)
+        } else {
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, 2)
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        if (requestCode == 1) {
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                val gallery =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+                startActivityForResult(gallery, 2)
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 2 && resultCode == Activity.RESULT_OK && data != null) {
+            selectedImage = data.data
+            if (selectedImage != null) {
+                val source =
+                    ImageDecoder.createSource(requireActivity().contentResolver, selectedImage!!)
+                selectedImageURI = ImageDecoder.decodeBitmap(source)
+                signImageView.setImageBitmap(selectedImageURI)
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
