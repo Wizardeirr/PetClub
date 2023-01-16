9d3bd12e673a0248e3250056dde5dfe1e3f41a1d
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2023-01-16 23:30:13 +0300

    RV Adapter almost done

--- app/src/main/java/com/volkankelleci/petsocialclub/adapter/PmRoomAdapter.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/adapter/PmRoomAdapter.kt
@@ -1,4 +1,22 @@
 package com.volkankelleci.petsocialclub.adapter
 
-class PmRoomAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PmRoomAdapter: RecyclerView.Adapter<PmRoomAdapter.PmRoomAdapterViewHolder>() {
+    class PmRoomAdapterViewHolder {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PmRoomAdapterViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PmRoomAdapterViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
