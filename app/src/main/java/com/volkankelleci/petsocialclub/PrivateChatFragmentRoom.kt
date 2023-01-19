735e1625eff0f62a251933ee862828ff185d2c3c
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2023-01-19 16:02:09 +0300

    privateMessage button working

--- app/src/main/java/com/volkankelleci/petsocialclub/PrivateChatFragmentRoom.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/PrivateChatFragmentRoom.kt
@@ -1,27 +1,16 @@
 package com.volkankelleci.petsocialclub
 
-import android.R
-import android.app.ActionBar
-import android.app.Activity
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.firebase.ui.auth.data.model.User
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.volkankelleci.petsocialclub.databinding.FragmentPrivateChatRoomBinding
 import com.volkankelleci.petsocialclub.util.UserInfo
 import com.volkankelleci.petsocialclub.util.Util
-import kotlinx.android.synthetic.main.fragment_private_chat_room.*
-import kotlinx.android.synthetic.main.fragment_user_chat.*
 
 
 class PrivateChatFragmentRoom : Fragment() {
@@ -51,31 +40,24 @@ class PrivateChatFragmentRoom : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
         binding.privateMessageSendButton.setOnClickListener {
-            val privateMessage = binding.privateMessageET.text.toString()
-            val gUser = Util.auth.currentUser!!.uid
-            val gdate = FieldValue.serverTimestamp()
-            val toId = userInfo!!.uuid
-
-
-
-            val privateChatDataMap = HashMap<String, Any>()
-            privateChatDataMap.put("PrivateChatGText", privateMessage)
-            privateChatDataMap.put("PrivateChatGUser", gUser)
-            privateChatDataMap.put("PrivateChatGDate", gdate)
-            privateChatDataMap.put("PrivateToID",toId)
-
-            firestore.collection("PrivateChats").add(privateChatDataMap).addOnSuccessListener {
-                binding.privateMessageET.setText("")
-            }.addOnFailureListener {
-                Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
-                binding.privateMessageET.setText("")
-            }
-
-
+            println("DONE")
         }
 
 
+    }
+    private fun privateChatInfoTake(){
+        val userUUID = Util.auth.currentUser!!.uid
+        val userEmail = Util.auth.currentUser!!.email.toString()
+
+
+        val userInfoMap = java.util.HashMap<String, Any>()
+        userInfoMap.put("PrivateChatUserUUID", userUUID)
+        userInfoMap.put("PrivateChatUserEmail", userEmail)
+        Util.database.collection("privateChat").add(userInfoMap).addOnSuccessListener {
+            Toast.makeText(requireContext(), "DONE", Toast.LENGTH_SHORT).show()
+        }
+            .addOnFailureListener {
+            }
     }
 }
