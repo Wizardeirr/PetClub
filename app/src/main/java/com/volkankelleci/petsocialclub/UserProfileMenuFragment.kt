7fbbff135b8f8cfba5e1e665968a757d736b4c4d
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2023-01-22 10:45:41 +0300

    View Binding added to UserProfileMenuFragment.kt

--- app/src/main/java/com/volkankelleci/petsocialclub/UserProfileMenuFragment.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/UserProfileMenuFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.volkankelleci.petsocialclub.databinding.FragmentUserProfileMenuBinding
 import com.volkankelleci.petsocialclub.util.UserInfo
 import com.volkankelleci.petsocialclub.util.Util
 
 class UserProfileMenuFragment : Fragment() {
+
+    private  var _binding:FragmentUserProfileMenuBinding?=null
+    private val binding get() = _binding!!
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +23,10 @@ class UserProfileMenuFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile_menu, container, false)
+        _binding=FragmentUserProfileMenuBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
