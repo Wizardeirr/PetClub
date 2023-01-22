24a7766601e44d8e200c38c72d3fd50f6c808dde
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2023-01-22 11:19:22 +0300

    Adapter integrated to UserProfileMenuFragment.kt

--- app/src/main/java/com/volkankelleci/petsocialclub/UserProfileMenuFragment.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/UserProfileMenuFragment.kt
@@ -5,14 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.volkankelleci.petsocialclub.adapter.UserProfileInfoAdapter
 import com.volkankelleci.petsocialclub.databinding.FragmentUserProfileMenuBinding
 import com.volkankelleci.petsocialclub.util.UserInfo
 import com.volkankelleci.petsocialclub.util.Util
+import kotlinx.android.synthetic.main.fragment_user_profile_menu.*
 
 class UserProfileMenuFragment : Fragment() {
 
-    private  var _binding:FragmentUserProfileMenuBinding?=null
+    private var _binding: FragmentUserProfileMenuBinding? = null
     private val binding get() = _binding!!
+    private lateinit var adapter: UserProfileInfoAdapter
+
+    var fragmentProfileInfoList = ArrayList<UserInfo>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,40 +32,39 @@ class UserProfileMenuFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View {
         // Inflate the layout for this fragment
-        _binding=FragmentUserProfileMenuBinding.inflate(inflater,container,false)
+        _binding = FragmentUserProfileMenuBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val userProfileName=binding.userNameETProfile.text.toString()
-        val userProfileEmail=binding.userSignEmail.text.toString()
-        val userProfilePassword=binding.passwordSignProfile.text.toString()
-        val userProfileImage=binding.signImageViewProfile
-        val userProfilePet=binding.petNameProfile.text.toString()
-
-
+        val layoutManager = LinearLayoutManager(activity)
+        userProfileInfoRV.layoutManager = layoutManager
+        adapter = UserProfileInfoAdapter(fragmentProfileInfoList)
+        userProfileInfoRV.adapter = adapter
 
     }
-    fun takesUserInfo(){
+
+    fun takesUserInfo() {
         Util.database.collection("userProfileInfo")
             .addSnapshotListener { value, error ->
-                if(error!=null){
-                }else
-                    if (value!=null){
-                        if (value.isEmpty==false){
-                            val documents=value.documents
-
-                            for (document in documents){
+                if (error != null) {
+                } else
+                    if (value != null) {
+                        if (value.isEmpty == false) {
+                            val documents = value.documents
+                            fragmentProfileInfoList.clear()
+                            for (document in documents) {
                                 document.get("userProfileInfo")
-                                val userEmail=document.get("userEmail").toString()
-                                val userUUID=document.get("userUUID").toString()
-                                val userName=document.get("userName").toString()
-                                val userPetName=document.get("petName").toString()
-                                val userImage=document.get("userImage").toString()
-                                val downloadInfos= UserInfo(userUUID,userEmail,userName,userPetName,userImage)
-
+                                val userEmail = document.get("userEmail").toString()
+                                val userUUID = document.get("userUUID").toString()
+                                val userName = document.get("userName").toString()
+                                val userPetName = document.get("petName").toString()
+                                val userImage = document.get("userImage").toString()
+                                val downloadInfos =UserInfo(userUUID, userEmail, userName, userPetName, userImage)
+                                fragmentProfileInfoList.add(downloadInfos)
                             }
+                            adapter.notifyDataSetChanged()
 
                         }
                     }
---
+++ app/src/main/java/com/volkankelleci/petsocialclub/adapter/UserProfileInfoAdapter.kt
@@ -0,0 +1,25 @@
+package com.volkankelleci.petsocialclub.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.volkankelleci.petsocialclub.util.UserInfo
+
+class UserProfileInfoAdapter(var profileInfoList:ArrayList<UserInfo>): RecyclerView.Adapter<UserProfileInfoAdapter.UserProfileInfoViewHolder>() {
+    class UserProfileInfoViewHolder(itemView:View):RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserProfileInfoViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: UserProfileInfoViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+
+    }
+}
