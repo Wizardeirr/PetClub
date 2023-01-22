ea28bd287a62ca4748848bfa23ebe10c9bff41ab
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2023-01-22 10:42:31 +0300

    All Profile UI names changed on xml

--- app/src/main/java/com/volkankelleci/petsocialclub/UserProfileMenuFragment.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/UserProfileMenuFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.volkankelleci.petsocialclub.util.UserInfo
+import com.volkankelleci.petsocialclub.util.Util
 
 class UserProfileMenuFragment : Fragment() {
 
@@ -24,6 +26,32 @@ class UserProfileMenuFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+    }
+    fun takesUserInfo(){
+        Util.database.collection("userProfileInfo")
+            .addSnapshotListener { value, error ->
+                if(error!=null){
+                }else
+                    if (value!=null){
+                        if (value.isEmpty==false){
+                            val documents=value.documents
+
+                            for (document in documents){
+                                document.get("userProfileInfo")
+                                val userEmail=document.get("userEmail").toString()
+                                val userUUID=document.get("userUUID").toString()
+                                val userName=document.get("userName").toString()
+                                val userPetName=document.get("petName").toString()
+                                val userImage=document.get("userImage").toString()
+                                val downloadInfos= UserInfo(userUUID,userEmail,userName,userPetName,userImage)
+
+                            }
+
+                        }
+                    }
+            }
     }
 
 
