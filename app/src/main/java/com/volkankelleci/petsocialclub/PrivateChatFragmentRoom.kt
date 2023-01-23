6de5c49b1955da4630b46407c98e5f8e8dad2f1d
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2023-01-23 18:36:39 +0300

    Private message data class created

--- app/src/main/java/com/volkankelleci/petsocialclub/PrivateChatFragmentRoom.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/PrivateChatFragmentRoom.kt
@@ -8,7 +8,9 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.volkankelleci.petsocialclub.databinding.FragmentPrivateChatRoomBinding
+import com.volkankelleci.petsocialclub.util.Post
 import com.volkankelleci.petsocialclub.util.UserInfo
 import com.volkankelleci.petsocialclub.util.Util
 import com.volkankelleci.petsocialclub.util.Util.database
@@ -20,6 +22,7 @@ class PrivateChatFragmentRoom : Fragment() {
     private var _binding:FragmentPrivateChatRoomBinding?=null
     private val binding get()=_binding!!
     private val user=UserInfo("","","","","","")
+    private val user=
 
     private lateinit var firestore: FirebaseFirestore
 
@@ -66,4 +69,32 @@ class PrivateChatFragmentRoom : Fragment() {
             .addOnFailureListener {
             }
     }
+    private fun takesData() {
+        database.collection("Post").orderBy("date", Query.Direction.DESCENDING)
+            .addSnapshotListener { value, error ->
+                if (error != null) {
+                } else
+                    if (value != null) {
+                        if (value.isEmpty == false) {
+                            val documents = value.documents
+                            postList.clear()
+                            for (document in documents) {
+                                document.get("Post")
+                                val userTitle = document.get("usertitle").toString()
+                                val userComment = document.get("usercomment").toString()
+                                val userImage = document.get("imageurl").toString()
+                                val userEmail = document.get("useremail").toString()
+
+                                val downloadInfos =
+                                    Post(userTitle, userComment, userImage, userEmail)
+                                postList.add(downloadInfos)
+
+                            }
+
+                        }
+                        recyclerViewAdapter.notifyDataSetChanged()
+                    }
+            }
+    }
+
 }
---
+++ app/src/main/java/com/volkankelleci/petsocialclub/util/PrivateMessage.kt
@@ -0,0 +1,7 @@
+package com.volkankelleci.petsocialclub.util
+
+import com.google.firebase.Timestamp
+import java.util.UUID
+
+class PrivateMessage(var message:String,var uuid:UUID,var timestamp: Timestamp, var chatUser:String) {
+}
