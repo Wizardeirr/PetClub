8eb0aa8a1d2bad1e8757b8b95f6514cedbad3db2
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2023-01-25 21:57:52 +0300

    saved

--- app/src/main/java/com/volkankelleci/petsocialclub/util/PrivateMessage.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/util/PrivateMessage.kt
@@ -2,7 +2,8 @@ package com.volkankelleci.petsocialclub.util
 
 class PrivateMessage(
     var message:String,
-    var uuid:String,
+    var fromUUID:String,
+    var toUUID:String,
     var timestamp: String,
     var chatUser:String) {
 }
