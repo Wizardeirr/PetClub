bd5db2bcaa81f8c039745c13d6d69a36fa880550
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2023-02-01 23:49:01 +0300

    adapter name changed

---
+++ app/src/main/java/com/volkankelleci/petsocialclub/adapter/PrivateMessageListAdapter.kt
@@ -0,0 +1,26 @@
+package com.volkankelleci.petsocialclub.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PrivateMessageListAdapter: RecyclerView.Adapter<PrivateMessageListAdapter.PrivateMessageListFragmentPart>() {
+    class PrivateMessageListFragmentPart(itemView: View):RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): PrivateMessageListFragmentPart {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PrivateMessageListFragmentPart, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
