b3d077194ae335e12c9f12071a6fa2f678f16157
volkan kelleci <kellecivolkan@gmail.com>
2023-09-14 06:06:15 +0300

    ArrayList Added to "PrivateMessage" model

--- app/src/main/java/com/volkankelleci/petsocialclub/data/PrivateMessage.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/data/PrivateMessage.kt
@@ -3,6 +3,6 @@ package com.volkankelleci.petsocialclub.data
 class PrivateMessage(
     var message:String,
     var fromUUID:String,
-    var toUUID:String,
+    var toUUID: ArrayList<String>,
     var timestamp: String,
     var chatUser:String)
