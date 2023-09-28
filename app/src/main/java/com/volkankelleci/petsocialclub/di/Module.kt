4e870a332fea51b5762b6c99477fc039ccaeac4b
volkan kelleci <kellecivolkan@gmail.com>
2023-09-28 20:40:17 +0300

    added, DI to manifest and Module

--- app/src/main/java/com/volkankelleci/petsocialclub/di/Module.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/di/Module.kt
@@ -1,5 +1,29 @@
 package com.volkankelleci.petsocialclub.di
 
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.volkankelleci.petsocialclub.room.UserUUIDDataBase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object Module {
+
+    @Singleton
+    @Provides
+    fun injectRoomDB(@ApplicationContext context:Context)= Room.databaseBuilder(context,
+    UserUUIDDataBase::class.java,"useruuiddb").build()
+
+    @Singleton
+    @Provides
+    fun injectDao(database: UserUUIDDataBase)=database.userDao()
+
+
 
 }
